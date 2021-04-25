f001b8eb2759e7635667227f706fde863634ddcd
Osmig Torres <torresosmig2020@gmail.com>
2021-04-24 21:29:41 -0700

    Initial Commit

---
+++ hey.py
@@ -0,0 +1,29 @@
+# clear terminal
+import os
+os.system('clear')
+
+#################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Boolean
+###################
+
+first_name = 'john'
+age = 41
+fav_pizza = {
+    "John": "Pepperoni",
+    "Bob": "Mushroom",
+    "Mary": "Cheese"
+    }
+name = True
+
+print(name)
+
+
+
+
+
+
